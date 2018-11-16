618e38a8a6e809a68d8684515cb37dc2dd2e0cf6
journeyman32 <bryan@coderbox.co>
2018-11-15 23:45:22 -0500

    moved from GoLand to MS Code. Code wise it was updating two function in ast that didnt have brackets and dding parser tests

--- ast/ast.go
+++ ast/ast.go
@@ -11,39 +11,39 @@ type Statement interface {
 	statementNode()
 }
 
-type Expression interface{
+type Expression interface {
 	Node
 	expressionNode()
 }
-type Program struct{
+type Program struct {
 	Statements []Statement
 }
 
-func (p *Program) TokenLiteral() string{
-	if len(p.Statements) > 0{
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
-	}else{
+	} else {
 		return ""
 	}
 }
 
-type LetStatement struct{
-	Token token.Token
-	Name *Identifier
+type LetStatement struct {
+	Token token.Token //the token.LET token
+	Name  *Identifier
 	Value Expression
 }
 
-func (ls *LetStatement) statementNode()
-func (ls *LetStatement) TokenLiteral() string{
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
-type Identifier struct{
-	Token token.Token
+type Identifier struct {
+	Token token.Token //the token.IDENT token
 	Value string
 }
 
-func (i *Identifier) expressionNode()
+func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
